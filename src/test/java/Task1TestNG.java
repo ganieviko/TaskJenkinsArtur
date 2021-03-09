8dcd934809fb6fcf9c596f8f59f9883a9110e5cd
username <ganieviko@gmail.com>
2021-03-08 21:38:45 -0500

    Artur change

--- src/test/java/Task1TestNG.java
+++ src/test/java/Task1TestNG.java
@@ -15,17 +15,11 @@ public class Task1TestNG {
     }
     @Test
     public void arutr3() {
-        boolean failOrNotFail = new Random().nextBoolean();
-        if(failOrNotFail) {
-            Assert.fail("Fail 3");
-        } else {
-            System.out.println("Success 3");
-        }
+
     }
 
     @Test
     public void arutr4() {
-        Assert.fail("Fail 4");
     }
 
 
